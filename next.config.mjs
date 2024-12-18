083136acf4023dbc41dabdadea91b03776ae2b75
shedi amara nebli <96447936+Sheedii@users.noreply.github.com>
2024-12-18 14:38:03 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        domains: ['example.com'],
-    },
-    i18n: {
-        locales: ['en', 'fr'],
-        defaultLocale: 'en',
+        output: 'export',
+    typescript: {
+        ignoreBuildErrors: true,
     },
 };
 
