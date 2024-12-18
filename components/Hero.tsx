d1774fb48ec37eca81078881d1f518fba22e4f77
shedi amara nebli <96447936+Sheedii@users.noreply.github.com>
2024-12-18 15:14:05 +0100

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ import { socialMedia } from "@/data";
 const Hero = () => {
   return (
     <div className="pb-20 pt-28" id="header" >
-      <div>
+      <div className="relative">
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
