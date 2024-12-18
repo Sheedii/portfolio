0d6508175980ba4011c9147d023d6e48d48b8079
shedi amara nebli <96447936+Sheedii@users.noreply.github.com>
2024-12-18 14:42:56 +0100

    Update route.js

--- app/api/sentry-example-api/route.js
+++ app/api/sentry-example-api/route.js
@@ -1,8 +1,1 @@
-import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
-export function GET() {
-  throw new Error("Sentry Example API Route Error");
-  return NextResponse.json({ data: "Testing Sentry Error..." });
-}
