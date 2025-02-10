39378665809ff94f3c02b5ed183150179f66b161
shedi amara nebli <96447936+Sheedii@users.noreply.github.com>
2025-02-10 10:59:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Head from "next/head";
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
@@ -13,16 +12,6 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
-     <>
-      <Head> {/* ✅ Correct usage in a client component */}
-        <title>Chadi's Portfolio</title>
-        <meta name="description" content="Welcome to my portfolio! Explore my projects and experience." />
-        <meta property="og:title" content="Chadi's Portfolio" />
-        <meta property="og:description" content="Discover my projects and skills as a FullStack Software Engineer." />
-        <meta property="og:image" content="https://your-image-url.com/preview.png" />
-        <meta property="og:url" content="https://sheedii.github.io/portfolio/" />
-        <meta property="og:type" content="website" />
-      </Head>
     <main className="bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
@@ -34,7 +23,6 @@ const Home = () => {
         <Footer />
       </div>
     </main>
-    </>
   );
 };
 
