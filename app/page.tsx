50a4acd11befd669ca9ecc41b1e79c9360e84039
shedi amara nebli <96447936+Sheedii@users.noreply.github.com>
2025-02-10 10:48:40 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,6 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 const Home = () => {
   return (
     <main className="bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <Head>
-        <title>Chadi's Portfolio</title>
-        <meta name="description" content="Welcome to my portfolio! Explore my projects and experience." />
-        <meta property="og:title" content="Chadi's Portfolio" />
-        <meta property="og:description" content="Discover my projects and skills as a FullStack Software Engineer." />
-        <meta property="og:url" content="https://sheedii.github.io/portfolio/" />
-      </Head>
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
